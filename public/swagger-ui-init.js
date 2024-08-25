da9e7d1f39aa3713972262e638a40146345c2907
JesusDHernandezR <jesusdavidhernandez@unicesar.edu.co>
2024-08-24 19:52:50 -0500

    swaggers

--- public/swagger-ui-init.js
+++ public/swagger-ui-init.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const ui = SwaggerUIBundle({
-      url: "", // Cambia esta URL si es necesario
+      url: "/api-docs/swagger.json", // Cambia esta URL si es necesario
       dom_id: '#swagger-ui',
       presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
       layout: "StandaloneLayout"
